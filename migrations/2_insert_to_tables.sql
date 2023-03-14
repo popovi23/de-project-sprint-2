79f7f41adf77aef7410d03ff33cf86ea4256adc3
popovi23 <123827822+popovi23@users.noreply.github.com>
2023-03-14 17:27:12 +0300

    fix format

--- migrations/2_insert_to_tables.sql
+++ migrations/2_insert_to_tables.sql
@@ -1,69 +1,69 @@
 -- Заполнение справочника стоимости доставки в страны
-insert into shipping_country_rates(
+INSERT INTO shipping_country_rates(
     shipping_country,
     shipping_country_base_rate
 )
-select distinct
+SELECT DISTINCT 
     s.shipping_country, s.shipping_country_base_rate
-from shipping s;
+FROM shipping s;
 
 
 -- Заполнение справочника тарифов доставки вендора по договору
-with description_array as (
-    select distinct
-        regexp_split_to_array(vendor_agreement_description, ':') as desc_arr
-    from shipping
+WITH description_array AS (
+    SELECT DISTINCT 
+        regexp_split_to_array(vendor_agreement_description, ':') AS desc_arr
+    FROM shipping
 )
-insert into shipping_agreement
-select
+INSERT INTO shipping_agreement
+SELECT
     desc_arr[1]::bigint,
     desc_arr[2],
     desc_arr[3]::numeric,
     desc_arr[4]::numeric
-from description_array;
+FROM description_array;
 
 
 -- Заполнение справочника о типах доставки
-with description_array as (
-    select distinct
-        regexp_split_to_array(shipping_transfer_description, ':') as desc_array,
+WITH description_array AS (
+    SELECT DISTINCT
+        regexp_split_to_array(shipping_transfer_description, ':') AS desc_array,
         shipping_transfer_rate
-    from shipping
+    FROM shipping
 )
-insert into shipping_transfer(
+INSERT INTO shipping_transfer(
     shipping_transfer_type,
     shipping_transfer_model,
     shipping_transfer_rate
 )
-select
+SELECT
     description_array.desc_array[1],
     description_array.desc_array[2],
     shipping_transfer_rate
-from description_array;
+FROM description_array;
 
 
 -- Заполнение таблицы с уникальными доставками
 
 -- 1. На время импорта данных удалим ограничения
-alter table shipping_info drop constraint if exists shipping_info_pkey;
-alter table shipping_info drop constraint if exists shipping_info_agreement_id_fkey;
-alter table shipping_info drop constraint if exists shipping_info_shipping_country_id_fkey;
-alter table shipping_info drop constraint if exists shipping_info_transfer_id_fkey;
+ALTER TABLE shipping_info DROP CONSTRAINT IF EXISTS shipping_info_pkey;
+ALTER TABLE shipping_info DROP CONSTRAINT IF EXISTS  shipping_info_agreement_id_fkey;
+ALTER TABLE shipping_info DROP CONSTRAINT IF EXISTS shipping_info_shipping_country_id_fkey;
+ALTER TABLE shipping_info DROP CONSTRAINT IF EXISTS shipping_info_transfer_id_fkey;
 
 -- 2. Импорт данных
-with sa as (
-    select distinct
+WITH sa AS (
+    SELECT DISTINCT
         shippingid,
         vendorid,
         payment_amount,
         shipping_plan_datetime,
-        regexp_split_to_array(shipping_transfer_description, ':') as transfer_description_arr,
-        regexp_split_to_array(vendor_agreement_description, ':') as agreement_description_arr,
+        regexp_split_to_array(shipping_transfer_description, ':') AS transfer_description_arr,
+        regexp_split_to_array(vendor_agreement_description, ':') AS agreement_description_arr,
         shipping_country
-    from shipping
+    FROM shipping
 )
-insert into shipping_info
-select
+INSERT INTO shipping_info
+SELECT
     shippingid,
     vendorid,
     payment_amount,
@@ -71,32 +71,32 @@ select
     st.shipping_transfer_id,
     sc.shipping_country_id,
     agreement_description_arr[1]::bigint
-from sa
-join shipping_transfer st ON
-    st.shipping_transfer_type  = sa.transfer_description_arr[1] and
+FROM sa
+JOIN shipping_transfer st ON
+    st.shipping_transfer_type  = sa.transfer_description_arr[1] AND
     st.shipping_transfer_model = sa.transfer_description_arr[2]
-join shipping_country_rates sc ON
+JOIN shipping_country_rates sc ON
     sc.shipping_country = sa.shipping_country;
 
 -- 3. Восстановим ограничения
-alter table shipping_info add constraint shipping_info_pkey
+ALTER TABLE shipping_info ADD constraint shipping_info_pkey
     PRIMARY KEY (shipping_id);
-alter table shipping_info add constraint shipping_info_agreement_id_fkey foreign key (agreement_id)
-    references shipping_agreement(agreement_id) on update cascade;
-alter table shipping_info add constraint shipping_info_shipping_country_id_fkey foreign key (shipping_country_id)
-    references shipping_country_rates(shipping_country_id) on update cascade;
-alter table shipping_info add constraint shipping_info_transfer_id_fkey foreign key (transfer_id)
-    references shipping_transfer(shipping_transfer_id) on update cascade;
+ALTER TABLE shipping_info ADD CONSTRAINT shipping_info_agreement_id_fkey FOREIGN KEY (agreement_id)
+    REFERENCES shipping_agreement(agreement_id) ON UPDATE CASCADE;
+ALTER TABLE shipping_info ADD CONSTRAINT shipping_info_shipping_country_id_fkey FOREIGN KEY (shipping_country_id)
+    REFERENCES  shipping_country_rates(shipping_country_id) ON UPDATE CASCADE;
+ALTER TABLE shipping_info ADD CONSTRAINT shipping_info_transfer_id_fkey FOREIGN KEY (transfer_id)
+    REFERENCES shipping_transfer(shipping_transfer_id) ON UPDATE CASCADE;
 
 
 -- Заполнение таблицы статусов о доставке
-insert into shipping_status
-    select distinct
+INSERT INTO shipping_status
+    SELECT DISTINCT
 	    shippingid,
-		max(state_datetime) filter(where state='booked') over idw shipping_start_fact_datetime,
-		max(state_datetime) filter(where state='recieved') over idw shipping_end_fact_datetime,
-		first_value(status) over max_sort max_status,
-		first_value(state) over max_sort max_state
-    from shipping
-    window  max_sort as (partition by shippingid order by state_datetime desc),
-    idw as (partition by shippingid);
+		max(state_datetime) FILTER(WHERE state='booked') OVER idw shipping_start_fact_datetime,
+		max(state_datetime) FILTER(WHERE state='recieved') OVER idw shipping_end_fact_datetime,
+		first_value(status) OVER max_sort max_status,
+		first_value(state) OVER max_sort max_state
+    FROM shipping
+    WINDOW max_sort AS (PARTITION BY shippingid ORDER BY state_datetime DESC),
+    idw AS (PARTITION BY shippingid);
